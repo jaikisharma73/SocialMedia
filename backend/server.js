34855825c24642fb525d703d861986e12563b7ca
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-09-19 12:36:20 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,6 @@ const start = async() => {
     const connectDB = await mongoose.connect("mongodb+srv://jack912062sharmas_db_user:0H6VzHqirDhDy8bS@linkedinclone.hdr1cib.mongodb.net/linkedinclone")
 }
 app.listen(9090 , ()=>{
-    console.log("Server is running on port 909");
+    console.log("Server is running on port 9090");
 })
 start();
