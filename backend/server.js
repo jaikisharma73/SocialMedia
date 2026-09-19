ae8769ec99162ed04029b7ba11e836453277fceb
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-09-19 12:25:45 +0530

    n

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const start = async() => {
+    const connectDB = await mongoose.connect("mongodb+srv://jack912062sharmas_db_user:0H6VzHqirDhDy8bS@linkedinclone.hdr1cib.mongodb.net/linkedinclone")
+}
+app.listen(9090 , ()=>{
+    console.log("Server is running on port 9090");
+})
+start();
