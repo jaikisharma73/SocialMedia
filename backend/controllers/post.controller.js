9409c7a5b97e36ba9651007e613962885ebdba33
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-09-21 10:58:38 +0530

    server

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,3 @@
+const activeCheck = (req, res) => {
+    return res.status(200).json({ message: "Active" });
+}
---
+++ backend/routes/post.routes.js
@@ -0,0 +1,7 @@
+import {Router} from 'express';
+const router = Router();
+
+
+router.route('/').get((req,res)=>{
+    res.send("Hello from the backend");
+}
