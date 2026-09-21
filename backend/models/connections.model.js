89a584cd1609a86df36f73a9d6410350f33fff30
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-09-21 19:57:36 +0530

    n

---
+++ backend/models/connections.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectionRequest = new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    connectionId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    status_accepted:{
+        type: Boolean,
+        default: null
+    }
+});
