52d691b1e0cd8422be0c9b5c3744aaf81e4df25e
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-09-25 22:28:13 +0530

    db done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,10 +1,11 @@
 import User from '../models/user.model.js';
 import Profile from '../models/profile.model.js';
 import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 
 
 
-const register = async (req, res) => {
+export const register = async (req, res) => {
     try{
          const { name, email, password, userName } = req.body;
          if(!name || !email || !password || !userName){
@@ -37,4 +38,34 @@ const register = async (req, res) => {
         error: error.message});
     }
 }
-export default register;
+
+export const login = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+         if(!email || !password){
+            return res.status(400).json({ message: "Email and password are required" });
+         }
+         const user = await User.findOne({
+            email: email
+         });
+         if(!user){
+            return res.status(404).json({ message: "Invalid credentials" });
+         }
+         const isMatch = await bcrypt.compare(password, user.password);
+         if(!isMatch){
+            return res.status(404).json({ message: "Invalid credentials" });
+         }
+         const token = crypto.randomBytes(32).toString('hex');
+
+         await User.updateOne({ _id: user._id }, { token: token });
+
+         return res.json({ token: token, message: "Login successful" });
+         
+    } catch(error){
+        console.error("LOGIN ERROR:", error);
+        return res.status(500).json({
+            message: "Server error",
+            error: error.message
+        });
+    }
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import register from '../controllers/user.controller.js';
+import {register , login} from '../controllers/user.controller.js';
 
 const router = Router();
 
 router.route('/register').post(register);
+router.route('/login').post(login);
 
-
-export default router;
+export default router;  
