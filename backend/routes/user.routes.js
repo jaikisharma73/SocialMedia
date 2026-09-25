23cfd60c4544cded73ccefcbea6ac74ce163e9b0
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-09-25 22:34:01 +0530

    multer continue

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,19 @@
 import { Router } from 'express';
 import {register , login} from '../controllers/user.controller.js';
+import multer from 'multer';
 
 const router = Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+  }});
+
+const upload = multer({ storage: storage });
+
 
 router.route('/register').post(register);
 router.route('/login').post(login);
